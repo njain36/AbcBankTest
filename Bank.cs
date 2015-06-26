54e8e618ebee1936953ead9939db9e406f814879
njain36 <njain36@hotmail.com>
2015-06-26 12:30:11 -0400

    AbcBankTest
    Abc Bank Test

---
+++ Bank.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AbcBankTest
+{
+
+    public class Bank
+    {
+        private List<Customer> customers;
+
+        public Bank()
+        {
+            customers = new List<Customer>();
+        }
+
+
+        public void addCustomer(Customer customer)
+        {
+            customers.Add(customer);
+        }
+
+
+        public String customerSummary()
+        {
+            String summary = "Customer Summary";
+            foreach (Customer c in customers)
+                summary += "\n - " + c.getName() + " (" + format(c.getNumberOfAccounts(), "account") + ")";
+            return summary;
+        }
+
+        private String format(int number, String word)
+        {
+            return number + " " + (number == 1 ? word : word + "s");
+        }
+
+
+        public double totalInterestPaid()
+        {
+            double total = 0;
+            foreach (Customer c in customers)
+                total += c.totalInterestEarned();
+            return total;
+        }
+
+
+        public String getFirstCustomer()
+        {
+            try
+            {
+                customers = null;
+                return customers[0].getName();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                return "Error";
+            }
+        }
+    }
+}
---
+++ Customer.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AbcBankTest
+{
+
+    public class Customer
+    {
+        private String name;
+        private List<Account> accounts;
+
+
+        public Customer(String name)
+        {
+            this.name = name;
+            this.accounts = new List<Account>();
+        }
+
+
+        public String getName()
+        {
+            return name;
+        }
+
+
+        public Customer openAccount(Account account)
+        {
+            accounts.Add(account);
+            return this;
+        }
+
+
+        public int getNumberOfAccounts()
+        {
+            return accounts.Count;
+        }
+
+
+        public double totalInterestEarned()
+        {
+            double total = 0;
+            foreach (Account a in accounts)
+                total += a.interestEarned();
+            return total;
+        }
+
+
+        public String getStatement()
+        {
+            String statement = null;
+            statement = "Statement for " + name + "\n";
+            double total = 0.0;
+            foreach (Account a in accounts)
+            {
+                statement += "\n" + statementForAccount(a) + "\n";
+                total += a.sumTransactions();
+            }
+            statement += "\nTotal In All Accounts " + toDollars(total);
+            return statement;
+        }
+
+
+        private String statementForAccount(Account a)
+        {
+            String s = "";
+
+            switch (a.getAccountType())
+            {
+                case Account.CHECKING:
+                    s += "Checking Account\n";
+                    break;
+                case Account.SAVINGS:
+                    s += "Savings Account\n";
+                    break;
+                case Account.MAXI_SAVINGS:
+                    s += "Maxi Savings Account\n";
+                    break;
+            }
+
+            double total = 0.0;
+            foreach (Transaction t in a.transactions)
+            {
+                s += "  " + (t.amount < 0 ? "withdrawal" : "deposit") + " " + toDollars(t.amount) + "\n";
+                total += t.amount;
+            }
+            s += "Total " + toDollars(total);
+            return s;
+        }
+
+
+        private String toDollars(double d)
+        {
+            return String.Format("${0:N2}", Math.Abs(d));
+        }
+    }
+}
