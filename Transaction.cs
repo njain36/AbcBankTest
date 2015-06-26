f121782a773ee7d0588ca009e2151d73c9c6277f
abcBank <njain36@hotmail.com>
2015-06-26 12:53:32 -0400

    AbcBankTest
    Abc Bank Test

---
+++ Transaction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AbcBankTest
+{
+    public class Transaction
+    {
+        public readonly double amount;
+
+        private DateTime transactionDate;
+
+        public Transaction(double amount)
+        {
+            this.amount = amount;
+            this.transactionDate = DateTime.Now;
+        }
+
+    }
+}
---
+++ TransactionTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+
+namespace AbcBankTest
+{
+    [TestFixture]
+    public class TransactionTest
+    {
+        [Test]
+        public void transaction()
+        {
+            Transaction t = new Transaction(5);
+            Assert.AreEqual(true, t is Transaction);
+        }
+    }
+}
